6a8930ecaf0966d3ce17806be504942e645a381c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-18 12:49:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,136 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let ADMINS = [];
+let USERS = [];
+let COURSES = [];
+
+
+//this is middleware
+const adminAuthentication = (req, res, next) => {
+    const {username, password} = req.headers;
+    // console.log("control here")
+    // console.log(username);
+    const admin = ADMINS.find(a => a.username === username && a.password === password);
+    if(admin) {
+        next();
+    } else {
+        res.status(403).json({ message: "Admin Authentication failed" });
+    }
+}
+
+const userAuthentication = (req, res, next) => {
+  const { username, password } = req.headers;
+  const user = USERS.find(u => u.username === username && u.password === password);
+  if (user) {
+    req.user = user;  // Add user object to the request
+    next();
+  } else {
+    res.status(403).json({ message: 'User authentication failed' });
+  }
+};
+// Admin routes
+app.post('/admin/signup', (req, res) => {
+  // logic to sign up admin
+    var admin = req.body;
+    let adminAlreadyExists = ADMINS.find((a) => a.username === admin.username);
+    
+    if(adminAlreadyExists) {
+        res.status(403).json({message: "User already exists"});
+    } else {
+        ADMINS.push(admin); 
+        res.status(200).json({message: "Admin Signup Successfully"});
+    } 
+});
+
+app.post('/admin/login', adminAuthentication, (req, res) => {
+  res.json({ message: "Admin logged in successfully" })
+  // logic to log in admin
+   
+});
+
+app.post('/admin/courses',  adminAuthentication,  (req, res) => {
+  // logic to create a course
+  const course = req.body;
+  course.id = Date.now();
+  COURSES.push(course);
+  res.json({message: "Course created successfully", courseId: course.id});
+
+});
+
+app.put('/admin/courses/:courseId', adminAuthentication, (req, res) => {
+  // logic to edit a course
+  const courseId = Number(req.params.courseId);
+  const course = COURSES.find(c => c.id === courseId);
+  if(course) {
+    Object.assign(course, req.body);
+    res.json({message: "Course updated successfully"});
+  }
+  else {
+    res.status(404).json({message: "Course not found"});
+  }
+});
+
+app.get('/admin/courses', adminAuthentication, (req, res) => {
+  // logic to get all courses
+  res.json({ courses: COURSES}) 
+
+})
+
+app.post('/users/signup', (req, res) => {
+  // const user = {...req.body, purchasedCourses: []};
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+    purchasedCourses: []
+  }
+  USERS.push(user);
+  res.json({ message: 'User created successfully' });
+});
+
+app.post('/users/login', userAuthentication, (req, res) => {
+  res.json({ message: 'Logged in successfully' });
+});
+
+app.get('/users/courses', userAuthentication, (req, res) => {
+  // COURSES.filter(c => c.published)
+  let filteredCourses = [];
+  for (let i = 0; i<COURSES.length; i++) {
+    if (COURSES[i].published) {
+      filteredCourses.push(COURSES[i]);
+    }
+  }
+  res.json({ courses: filteredCourses });
+});
+
+app.post('/users/courses/:courseId', userAuthentication, (req, res) => {
+  const courseId = Number(req.params.courseId);
+  const course = COURSES.find(c => c.id === courseId && c.published);
+  if (course) {
+    req.user.purchasedCourses.push(courseId);
+    res.json({ message: 'Course purchased successfully' });
+  } else {
+    res.status(404).json({ message: 'Course not found or not available' });
+  }
+});
+
+app.get('/users/purchasedCourses', userAuthentication, (req, res) => {
+  // const purchasedCourses = COURSES.filter(c => req.user.purchasedCourses.includes(c.id));
+  // We need to extract the complete course object from COURSES
+  // which have ids which are present in req.user.purchasedCourses
+  var purchasedCourseIds = req.user.purchasedCourses; [1, 4];
+  var purchasedCourses = [];
+  for (let i = 0; i<COURSES.length; i++) {
+    if (purchasedCourseIds.indexOf(COURSES[i].id) !== -1) {
+      purchasedCourses.push(COURSES[i]);
+    }
+  }
+
+  res.json({ purchasedCourses });
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+})  
