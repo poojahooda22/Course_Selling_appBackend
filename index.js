8c6b944bdeeb6d4dc16b27bef79d67807106d644
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-18 12:59:57 +0530

    updated index

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const userAuthentication = (req, res, next) => {
 };
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // lo
+  //
     var admin = req.body;
     let adminAlreadyExists = ADMINS.find((a) => a.username === admin.username);
     
