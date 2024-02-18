567c5bbb1ebae169c1fae817b435d080de6d419f
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-18 12:55:09 +0530

    updated index

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let COURSES = [];
 //this is middleware
 const adminAuthentication = (req, res, next) => {
     const {username, password} = req.headers;
-    // console.log("co
+    // console.log("
     const admin = ADMINS.find(a => a.username === username && a.password === password);
     if(admin) {
         next();
