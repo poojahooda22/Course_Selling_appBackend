dcf1ea04f302c7fafacba87e9208dfdec195a25b
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-02-21 11:34:12 +0530

    updated course

--- course_app_hard.js
+++ course_app_hard.js
@@ -9,7 +9,7 @@ const SECRET = "my-secret-key";
 
 //define mongoose schemas
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: {type: String},
     password: String,
     purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course'}]
 });
@@ -58,7 +58,6 @@ mongoose.connect("mongodb+srv://phooda938:phooda938@cluster0.kof5uvs.mongodb.net
 app.post('/admin/signup', async (req, res) => {
     const {username, password} = req.body;
     const admin = await Admin.findOne({ username});
-
     if(admin) {
         res.status(403).json({message: 'Admin already exists'});
     } else {
@@ -67,5 +66,95 @@ app.post('/admin/signup', async (req, res) => {
         const token = jwt.sign({username, role: 'admin'}, SECRET, {expiresIn: '1h'});
         res.json({message: 'Admin created successfully', token});
     }
+});
 
+app.post('/admin/login', async(req, res) => {
+    const {username, password} = req.headers;
+    const admin = await Admin.findOne({username, password});
+    if(admin) {
+        const token = jwt.sign({username, role: 'admin'}, SECRET, {expiresIn: '1h'});
+        res.json({message: 'Logged in successfully', token});
+    } else {
+        res.status(403).json({message: 'Invalid or password'});
+    }
+});
+
+
+app.post('/admin/courses', authenticateJwt, async (req, res) => {
+    const course = new Course(req.body);
+    await course.save();
+    res.json({message: 'Course created successfully', courseId: course.id });    
 })
+
+app.put('/admin/courses/:courseId', authenticateJwt, async (req, res) => {
+    const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, { new: true});
+    if(course) {
+        res.json({message: 'course updated successfully'});
+    } else {
+        res.status(404).json({ message: 'course not found'});
+    }
+});
+
+app.get('admin/courses', authenticateJwt, async(req, res) => {
+    const courses = await Course.find({});
+
+    res.json({courses});
+});
+
+//user routes
+
+app.post('/users/signup', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({ username});
+    if(user) {
+        res.status(403).json({message: 'User already exists'});
+    } else {
+        const newUser = new User({ username, password});
+        await newUser.save();
+        const token = jwt.sign({username, role: 'user'}, SECRET, {expiresIn: '1h'});
+        res.json({message: 'User created successfully', token});
+    }
+});
+
+app.post('/users/login', async(req, res) => {
+    const {username, password} = req.headers;
+    const user = await User.findOne({username, password});
+    if(user) {
+        const token = jwt.sign({username, role: 'user'}, SECRET, {expiresIn: '1h'});
+        res.json({message: 'logged in seccessfully ', token});
+    } else {
+        res.status(403).json({message: 'Invalid username or password'});
+    }
+})
+
+app.get('/users/courses', authenticateJwt, async (req, res) => {
+    const courses = await Course.find({published: true});
+    res.json({ courses });
+  });
+
+app.post('/users/courses/:courseId', authenticateJwt, async(req, res) => {
+    const course = await Course.findById(req.params.courseId);
+    if(course) {
+        const user = await User.findOne({username: rq.user.username});
+        if(user) {
+            user.purchasedCourses.push(course);
+            await user.save();
+            res.json({message: 'course purchased successfully!'});
+        } else {
+            res.status(403).json({message: 'user not found'});
+        }
+    } else {
+        res.status(404).json({message: 'course not found'})
+    }
+});
+
+app.get('/users/purchasedCourses', authenticateJwt, async (req, res) => {
+    const user = await User.findOne({username: req.user.username}).populate('purchasedCourses');
+    if(user) {
+        res.json({ purchasedCourses: user.purchasedCourses || [] });
+    } else {
+        res.status(403).json({message: 'user not found'});
+    }
+})
+
+app.listen(3000, () => console.log('Server running on port 3000'));
